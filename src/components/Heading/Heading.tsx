2e849028939975ed115681e0c52ef1771e182de3
udayakumar9779 <udayiwov@gmail.com>
2025-03-13 05:21:45 +0800

    Testing Husky

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -5,7 +5,7 @@ interface HeadingProps {
 }
 
 const Heading: React.FC<HeadingProps> = ({ text }) => {
-  return <h2 className="heading pageheading">{text}</h2>;
+  return <h2 className="heading">{text}</h2>;
 };
 
 export default Heading;
