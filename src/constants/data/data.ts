3156dd110b2d531eb596345e927245a3dcae504a
udayakumar9779 <udayiwov@gmail.com>
2025-03-13 05:06:47 +0800

    Testing Husky

--- src/constants/data/data.ts
+++ src/constants/data/data.ts
@@ -7,7 +7,7 @@ interface PageContent {
 const pageContent: PageContent = {
   h1: '  Unlocking Potential Through Strategy',
   h3: ' Empowering Growth & Innovation',
-  p: 'Success is test built on strategic vision, adaptability, and innovation. By leveraging data-driven insights and industry best practices, businesses can navigate challenges and drive sustainable growth',
+  p: 'Success is built on strategic vision, adaptability, and innovation. By leveraging data-driven insights and industry best practices, businesses can navigate challenges and drive sustainable growth',
 };
 
 export default pageContent;
